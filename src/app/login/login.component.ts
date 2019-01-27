7e6e5a0adeb3417d906c9d5ced2ff82e4b95a013
heysyam99 <35216709+heysyam99@users.noreply.github.com>
2019-01-27 23:18:43 +0800

    Update logic to the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Student } from '../student';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  student = new Student();
+
+  constructor(
+    private studentService: StudentService,
+    private router: Router,
+    private location: Location
+  ) { }
+
+
+  id: string;
+  password: string;
 
   ngOnInit() {
   }
 
+  async login(pelajar: Student) {
+
+    const result = await this.studentService.getStudent(pelajar.id, pelajar.password);
+    console.log(result);
+
+    if (result) {
+      this.router.navigate(["user"]);
+      return;
+    }
+    alert("Invalid");
+  }
 }
+
